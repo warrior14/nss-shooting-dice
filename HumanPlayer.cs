a9b150b8fbf8c963cc897fe554659698dd9f7468
warrior14 <ironaddicts14@gmail.com>
2021-09-03 20:11:14 -0500

    finished with shooting dice exxercise

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,44 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
+        public override int Roll()
+        {
+            // tell the user to input a number for roll:
+            Console.Write($"{Name}, says please enter a number for a roll between 1 and {DiceSize}!");
+            Console.WriteLine();
+            Console.Write("User Number: ");
+            int userInput = int.Parse(Console.ReadLine());
+            return userInput;
+        }
 
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            Console.WriteLine($"{Name} prompted the user to enter a number for a roll! :D");
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
